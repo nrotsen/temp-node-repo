06bf753af1cca38bac9e112da3b417a44d4c9e34
Nestor <nestor.alive@gmail.com>
2024-03-10 17:59:16 -0300

    unit 1, 2 and 3 done

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,7 +1,7 @@
 const { readFileSync, writeFileSync } = require("fs");
 console.log("start");
-const first = readFileSync("./content/first.txt", "utf8");
 
+const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 
 writeFileSync(
